e24460adbd2ffde50501688d77535df4b2093ccb
Beteab <bebidems08@gmail.com>
2023-09-23 15:40:01 -0500

    update

--- app/contractors/create/page.tsx
+++ app/contractors/create/page.tsx
@@ -16,11 +16,10 @@ const App = () => {
       </div>
       <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
         <div className="mb-4 flex flex-col gap-6">
-          <Input size="lg" label="First Name" crossOrigin="anonymous" />
-          <Input size="lg" label="Last Name" crossOrigin="anonymous" />
           <Input size="lg" label="Company Name" crossOrigin="anonymous" />
           <Input size="lg" label="Company Email" crossOrigin="anonymous" />
-          <Input size="lg" label="Company Address" crossOrigin="anonymous" />
+          <Input size="lg" label="Company Phone" crossOrigin="anonymous" />
+          <Input size="lg" label="Location ID" crossOrigin="anonymous" />
           <Input size="lg" label="City" crossOrigin="anonymous" />
           <Input size="lg" label="State" crossOrigin="anonymous" />
           <Input size="lg" label="Zipcode" crossOrigin="anonymous" />
