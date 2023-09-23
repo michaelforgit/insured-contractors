dae73eb6f18c3b3bfd7f29d5ab4f2055d0cfc119
Beteab <bebidems08@gmail.com>
2023-09-23 14:28:47 -0500

    update

--- app/contractors/[id]/edit/page.tsx
+++ app/contractors/[id]/edit/page.tsx
@@ -1,131 +1,32 @@
 'use client';
 import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 
+const App = () => {
+  return (
+    <Card color="transparent" shadow={false}> 
+      <Typography color="gray" className="mt-1 font-normal">
+        Contractor Registration
+      </Typography>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <div className="mb-4 flex flex-col gap-6">
+          <Input size="lg" label="Name" crossOrigin="anonymous" />
+          <Input size="lg" label="Email" crossOrigin="anonymous" />
+          <Input type="password" size="lg" label="Password" crossOrigin="anonymous" />
+        </div>
 
-const onFinish = (values: any) => {
-  console.log('Success:', values);
-};
-
-const onFinishFailed = (errorInfo: any) => {
-  console.log('Failed:', errorInfo);
-};
-
-type FieldType = {
-  FirstName?: string;
-  LastName?: string;
-  CompanyEmail?: string;
-  CompanyName?: string;
-  CompanyAddress?: string;
-  City?:string;
-  State?:string;
-  ZipCode?:Number;
-  InsureName?:string;
-  InsureEmail?:string;
-  InsurePhone?:Number;
-};
-
-const App: React.FC = () => (
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="FirstName"
-      name="FirstName"
-      rules={[{ required: true, message: 'Please input your First Name: ' }]}
-    >
-      <Input />
-    </Form.Item>
-
-
-    <Form.Item<FieldType>
-      label="LastName"
-      name="LastName"
-      rules={[{ required: true, message: 'Please input your password: ' }]}
-    >
-      <Input />
-    </Form.Item>
-  
-
-    <Form.Item<FieldType>
-      label="CompanyEmail"
-      name="CompanyEmail"
-      rules={[{ required: true, message: 'Please input your CompanyEmail: ' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="CompanyName"
-      name="CompanyName"
-      rules={[{ required: true, message: 'Please input your CompanyName: ' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="CompanyAddress"
-      name="CompanyAddress"
-      rules={[{ required: true, message: 'Please input your CompanyAddress: ' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="City"
-      name="City"
-      rules={[{ required: true, message: 'Please input your City: ' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="State"
-      name="State"
-      rules={[{ required: true, message: 'Please input your State: ' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="InsureName"
-      name="InsureName"
-      rules={[{ required: true, message: 'Please input your InsureName: ' }]}
-    >
-      <Input />
-    </Form.Item>
-
-
-    <Form.Item<FieldType>
-      label="InsureEmail"
-      name="InsureEmail"
-      rules={[{ required: true, message: 'Please input your Insurers Email: ' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="InsurePhone"
-      name="InsurePhone"
-      rules={[{ required: true, message: 'Please input your Insurers Phone Number: ' }]}
-    >
-      <Input />
-    </Form.Item>
-    
-
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        update
-      </Button>
-    </Form.Item>
-  </Form>
-);
+        <Button className="mt-6" fullWidth>
+          Submit
+        </Button>
+      </form>
+    </Card>
+  );
+}
 
 export default App;
