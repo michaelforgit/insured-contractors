0337e8b97d6b45317fee28fca408a4951136636c
Deng-99 <deng.cuh19@gmail.comgit config -- user.name>
2023-09-23 17:06:18 -0500

    UpdateCss

--- app/contractors/create/page.tsx
+++ app/contractors/create/page.tsx
@@ -10,22 +10,22 @@ import {
 
 const App = () => {
   return (
-    <div className="grid justify-center">
-      <div color="gray" className="mt-1 font-normal">
+    <div className="grid justify-center  text-black truncate bg-white" >
+      <div color="gray" className="mt-1 font-normal text-3xl mb-px[10px] py-6">
         Contractor Registration
       </div>
       <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-        <div className="mb-4 flex flex-col gap-6">
-          <Input size="lg" label="Company Name" crossOrigin="anonymous" />
-          <Input size="lg" label="Company Email" crossOrigin="anonymous" />
-          <Input size="lg" label="Company Phone" crossOrigin="anonymous" />
-          <Input size="lg" label="Location ID" crossOrigin="anonymous" />
-          <Input size="lg" label="City" crossOrigin="anonymous" />
-          <Input size="lg" label="State" crossOrigin="anonymous" />
-          <Input size="lg" label="Zipcode" crossOrigin="anonymous" />
-          <Input size="lg" label="Insurance Name" crossOrigin="anonymous" />
-          <Input size="lg" label="Insurance Email" crossOrigin="anonymous" />
-          <Input size="lg" label="Insurance Phone" crossOrigin="anonymous" />
+        <div className="mb-4 flex flex-col gap-6 pt-6 text-black truncate">
+          <Input size="lg" label="Company Name" crossOrigin="anonymous " className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"/>     
+        <Input size="lg" label="Company Email" crossOrigin="anonymous" className='peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'/>
+          <Input size="lg" label="Company Phone" crossOrigin="anonymous" className='peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'/>
+          <Input size="lg" label="Location ID" crossOrigin="anonymous" className='peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50' />
+          <Input size="lg" label="City" crossOrigin="anonymous" className='peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'/>
+          <Input size="lg" label="State" crossOrigin="anonymous" className='peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'/>
+          <Input size="lg" label="Zipcode" crossOrigin="anonymous" className='peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'/>
+          <Input size="lg" label="Insurance Name" crossOrigin="anonymous" className='peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'/>
+          <Input size="lg" label="Insurance Email" crossOrigin="anonymous" className='peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'/>
+          <Input size="lg" label="Insurance Phone" crossOrigin="anonymous" className='peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'/>
         </div>
 
         <Button className="mt-6" fullWidth>
