722c514fa0b137c47a3ad850fb901375138705f3
Beteab <bebidems08@gmail.com>
2023-09-23 22:40:37 -0500

    update

--- app/contractors/create/page.tsx
+++ app/contractors/create/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import clientPromise from '../../lib/mongodb'
-
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 const contractorServices = [
   "Plumbing",
   "Electrical",
@@ -117,16 +123,43 @@ const contractorServices = [
   "Tile",
 ];
 
-export default async function Create() {
+type contractorInformation = {
+  company: string;
+  locationId: string;
+  email: string;
+  phoneNumber: string;
+  city: string;
+  state: string;
+  zip: string;
+  insuranceName: string;
+  insurancePhone: string;
+  insuranceEmail: string;
+}
 
-  try {
+export default async function Create() {
+  const testDoc = {
+    id: "99000909090",
+    company: "BM Toe Truck",
+    locationId: "1234567890",
+    email: "blahblah@gmail.com",
+    phoneNumber: "1234567890",
+    city: "San Francisco",
+    state: "CA",
+    zip: "94103",
+    insuranceName: "Geico",
+    insurancePhone: "1234567890",
+    insuranceEmail: "support@geico.com",
+  };
+
+
+ try {
     const client = await clientPromise;
     const db = await client.db("insured-contractors");
-    const contractorsReq = await db.collection('contractors').find({}).toArray();
-    console.log(contractorsReq);
   } catch (error) {
     console.log(error)
   }
+  //dummy data
+
 
   return (
     <div className="grid justify-center  text-black truncate bg-white p-4" >
@@ -136,18 +169,89 @@ export default async function Create() {
       <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
         <div className="mb-4 flex flex-col gap-6 pt-6 text-black truncate">
           <div>
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-              Username
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="company">
+              Company Name
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Company" type="text"/>
           </div>
           <div>
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="test">
-              Test
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="locationId">
+              Location ID
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="locationId" type="text"/>
           </div>
+          <div>
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text"/>
+          </div>
+          <div>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="city">
+            City
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="city"
+            type="text"
+          />
         </div>
+
+        <div>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="state">
+            State
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="state"
+            type="text"
+          />
+        </div>
+
+        <div>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="zip">
+            Zip Code
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="zip"
+            type="text"
+          />
+        </div>
+
+        <div>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="insuranceName">
+            Insurance Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="insuranceName"
+            type="text"
+          />
+        </div>
+
+        <div>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="insurancePhone">
+            Insurance Phone
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="insurancePhone"
+            type="text"
+          />
+        </div>
+
+        <div>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="insuranceEmail">
+            Insurance Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="insuranceEmail"
+            type="text"
+          />
+        </div>
+      </div>
         
         <div className="w-full">
           <label className="inline-block text-sm text-gray-600" htmlFor="Multiselect">
@@ -169,7 +273,10 @@ export default async function Create() {
             </select>
           </div>
         </div>
-
+        
+        <button 
+          className="w-full mt-8 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+        > Submit </button>
         
       </form>
     </div>
