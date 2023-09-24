af8a9e6941cb26800c12b7a2cf35f704d322f469
Beteab <bebidems08@gmail.com>
2023-09-24 09:06:22 -0500

    update

--- app/contractors/[id]/edit/page.tsx
+++ app/contractors/[id]/edit/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect, use } from 'react';
+import React, { useState, useEffect} from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { ObjectId } from 'mongodb';
 
