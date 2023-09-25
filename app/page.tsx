f36febace9b821dedce2368cf3b0157f8ce42834
Michael <michaelforemail@protonmail.com>
2023-09-25 18:33:29 -0500

    Don't cache request for contractor data

--- app/page.tsx
+++ app/page.tsx
@@ -25,6 +25,7 @@ export default async function Home() {
     headers: {
       'Content-Type': 'application/json'
     },
+    cache: 'no-cache',
   }).then( async (data) => {
     return await data.json();
   } ). catch( (error) => {
